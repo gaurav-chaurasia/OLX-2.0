2e66835653abaa4e230fe1b68c200e02e1ef3ae1
gaurav-chaurasia <gaurav.181cv155@nitk.edu.in>
2020-01-26 01:13:13 +0530

    user table added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-    
+    validates :username, presence: true, uniqueness: {case_sensitive: false }
+    validates :email, presence: true, uniqueness: {case_sensitive: false }
 end
