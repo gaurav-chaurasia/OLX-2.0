73136cab9f16e3a2a194a49cb791d9e3be8dd6b1
gaurav-chaurasia <gaurav.181cv155@nitk.edu.in>
2020-01-26 19:26:27 +0530

    login, signup, logut working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+
     def index
         @users = User.all
     end
@@ -18,10 +20,8 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account was successfully updated"
             redirect_to products_path
@@ -31,11 +31,14 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
